d35cfd536613a3f33795cfa9a36d07572ef000aa
BuddhaL1T3 <jmriggs75@gmail.com>
2018-06-11 11:58:38 -0400

    update

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://Dev:Dev1987@ds255889.mlab.com:55889/dev-noteful-app',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful-test',
+  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb://Dev:Dev1987@ds255889.mlab.com:55889/dev-noteful-app',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test'
 };
